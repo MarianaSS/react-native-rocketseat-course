6ae243b8931e8fd9337c09b2c68b1e42e4748d7d
Mariana Santana <sansilmari2@gmail.com>
2020-09-15 17:50:25 -0300

    upload do projeto

---
+++ src/config/StatusBarConfig.js
@@ -0,0 +1,4 @@
+import { StatusBar } from "react-native";
+
+StatusBar.setBackgroundColor("#DA552F"); // Para alterar a cor da status bar do Android
+StatusBar.setBarStyle("light-content"); // Para o iOS
---
+++ src/index.js
@@ -0,0 +1,8 @@
+ import React from 'react';
+ import Routes from './routes';
+ import "./config/StatusBarConfig"
+
+//arrow function: outra forma de declarar um componente 
+const App = () => <Routes />;
+
+export default App;
